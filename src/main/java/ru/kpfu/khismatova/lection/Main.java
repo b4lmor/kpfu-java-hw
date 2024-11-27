0528861b232b35915845ddd24d73695a24cee8f1
mistaken <imcomeinso2@gmail.com>
2024-11-27 09:08:19 +0300

    Update

--- src/main/java/ru/kpfu/khismatova/lection/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.kpfu.khismatova.lection;
-
-public class Main {
-    public static void main(String[] args) {
-        System.setProperty("MainFrame.encoding", "UTF-8");
-        var wnd = new MainFrame();
-        wnd.setVisible(true);
-    }
-}
