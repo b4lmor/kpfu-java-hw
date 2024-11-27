c398f987d3a2ed2ae99caea1d8edd4e816291fb3
a.lisitsin <a.lisitsin@g.nsu.ru>
2024-11-27 12:37:12 +0700

    lab5 | add doc

--- src/main/java/ru/kpfu/khismatova/lab5/math/Lagrange.java
+++ src/main/java/ru/kpfu/khismatova/lab5/math/Lagrange.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 public class Lagrange extends Polynomial implements Interpolation {
 
+    /**
+     * Интерполяция с использованием метода Лагранжа.
+     * <p>
+     * Этот метод вычисляет интерполяционный полином Лагранжа для заданного списка точек.
+     * Он суммирует l_i * y_i для каждой точки, чтобы сформировать итоговый полином.
+     *
+     * @param points Список точек, представляющих данные для интерполяции.
+     */
     @Override
     public void interpolate(List<Point> points) {
         Polynomial polynomial = new Polynomial();
@@ -16,6 +24,20 @@ public class Lagrange extends Polynomial implements Interpolation {
         this.coeffs = polynomial.getCoeffs();
     }
 
+    /**
+     * Вычисляет l_i для заданного индекса.
+     * <p>
+     * Этот метод создает полином l_i, который представляет собой произведение
+     * (x - x_j) / (x_i - x_j) для всех j, не равных i.
+     * <p>
+     * P.S.: дробь (x - x_j) / (x_i - x_j) представляется в виде полинома:
+     * <p>
+     * P(x) = x^1 * (1 / (x_i - x_j)) + x^0 * (x_j / (x_i - x_j))
+     *
+     * @param i Индекс точки, для которой нужно вычислить базисный полином.
+     * @param table Список точек, представляющих данные для интерполяции.
+     * @return Полином, представляющий базисный полином Лагранжа l_i.
+     */
     private Polynomial getL(int i, List<Point> table) { // = l_i
         double x_i = table.get(i).x;
         Polynomial polynomial = new Polynomial(1.);
