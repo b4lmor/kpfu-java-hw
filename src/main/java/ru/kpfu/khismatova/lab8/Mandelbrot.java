4e19df1e288c610864c3949ecb9eb95c96798f0a
Artem L. 'Fox <foxb4lmor@gmail.com>
2025-01-18 17:21:48 +0700

    lab8

--- src/main/java/ru/kpfu/khismatova/lab8/Mandelbrot.java
+++ src/main/java/ru/kpfu/khismatova/lab8/Mandelbrot.java
@@ -2,7 +2,9 @@ package ru.kpfu.khismatova.lab8;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.stream.IntStream;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Mandelbrot {
 
@@ -12,6 +14,8 @@ public class Mandelbrot {
 
     private final int height;
 
+    private final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
     public Mandelbrot(int width, int height) {
         this.width = width;
         this.height = height;
@@ -20,32 +24,70 @@ public class Mandelbrot {
     public BufferedImage drawMandelbrotSet(int power, int maxIter, float brightness) {
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        IntStream.range(0, width).parallel().forEach(
-                x -> {
-                    for (int y = 0; y < height; y++) {
-                        Complex z = new Complex(0, 0);
+        CountDownLatch latch = new CountDownLatch(width);
 
-                        Complex c = new Complex(
-                                (x - width / SIZE) * SIZE * SIZE / width,
-                                (y - height / SIZE) * SIZE * SIZE / height
-                        );
+        for (int x = 0; x < width; x++) {
+            int finalX = x;
+            executor.submit(() -> {
+                calculateColumn(finalX, image, power, maxIter, brightness);
+                latch.countDown();
+            });
+        }
 
-                        int iter = 0;
-
-                        while (z.magnitudeSquared() < SIZE * SIZE && iter < maxIter) {
-                            z = z.power(power).add(c);
-                            iter++;
-                        }
-
-                        int color = (iter == maxIter)
-                                ? 0
-                                : Color.HSBtoRGB((float) iter / maxIter, 0.7f, brightness);
-
-                        image.setRGB(x, y, color);
-                    }
-                }
-        );
+        try {
+            latch.await();
+        } catch (InterruptedException ignored) {
+        }
 
         return image;
+    }
+
+    private void calculateColumn(int x, BufferedImage image, int power, int maxIter, float brightness) {
+        for (int y = 0; y < height; y++) {
+            Complex z = new Complex(0, 0);
+
+            Complex c = new Complex(
+                    (x - width / SIZE) * SIZE * SIZE / width,
+                    (y - height / SIZE) * SIZE * SIZE / height
+            );
+
+            int iter = 0;
+
+            while (z.magnitudeSquared() < SIZE * SIZE && iter < maxIter) {
+                z = z.power(power).add(c);
+                iter++;
+            }
+
+            int color = (iter == maxIter)
+                    ? 0
+                    : Color.HSBtoRGB((float) iter / maxIter, 0.7f, brightness);
+
+            image.setRGB(x, y, color);
+        }
+    }
+
+    record Complex(double real, double imaginary) {
+        public Complex add(Complex other) {
+            return new Complex(this.real + other.real, this.imaginary + other.imaginary);
+        }
+
+        public Complex multiply(Complex other) {
+            return new Complex(this.real * other.real - this.imaginary * other.imaginary,
+                    this.real * other.imaginary + this.imaginary * other.real);
+        }
+
+        public double magnitudeSquared() {
+            return real * real + imaginary * imaginary;
+        }
+
+        public Complex power(int exponent) {
+            Complex result = new Complex(1, 0);
+
+            for (int i = 0; i < exponent; i++) {
+                result = result.multiply(this);
+            }
+
+            return result;
+        }
     }
 }
